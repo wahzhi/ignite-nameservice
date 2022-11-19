f92a37547283d3733a7998c84af9247cfbc88855
wahzhi <fre51bor@gmail.com>
2022-11-19 15:33:13 -0800

    Added whois

--- go.mod
+++ go.mod
@@ -5,14 +5,21 @@ go 1.16
 require (
 	github.com/cosmos/cosmos-sdk v0.45.4
 	github.com/cosmos/ibc-go/v3 v3.0.0
+	github.com/gogo/protobuf v1.3.3
+	github.com/golang/protobuf v1.5.2
+	github.com/gorilla/mux v1.8.0
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0
 	github.com/ignite/cli v0.22.2
 	github.com/spf13/cast v1.4.1
+	github.com/spf13/cobra v1.4.0
 	github.com/stretchr/testify v1.7.1
 	github.com/tendermint/spn v0.2.1-0.20220610090138-44b136f042c4
 	github.com/tendermint/tendermint v0.34.19
 	github.com/tendermint/tm-db v0.6.7
-	golang.org/x/net v0.1.0 // indirect
-	google.golang.org/genproto v0.0.0-20221027153422-115e99e71e1c // indirect
+	golang.org/x/net v0.2.0 // indirect
+	google.golang.org/genproto v0.0.0-20221114212237-e4508ebdbee1
+	google.golang.org/grpc v1.50.1
+	gopkg.in/yaml.v2 v2.4.0
 )
 
 replace (
---
+++ vue/src/store/generated/nameservice/nameservice.nameservice/module/index.ts
@@ -0,0 +1,66 @@
+// THIS FILE IS GENERATED AUTOMATICALLY. DO NOT MODIFY.
+
+import { StdFee } from "@cosmjs/launchpad";
+import { SigningStargateClient } from "@cosmjs/stargate";
+import { Registry, OfflineSigner, EncodeObject, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
+import { Api } from "./rest";
+import { MsgSetName } from "./types/nameservice/tx";
+import { MsgDeleteName } from "./types/nameservice/tx";
+import { MsgBuyName } from "./types/nameservice/tx";
+
+
+const types = [
+  ["/nameservice.nameservice.MsgSetName", MsgSetName],
+  ["/nameservice.nameservice.MsgDeleteName", MsgDeleteName],
+  ["/nameservice.nameservice.MsgBuyName", MsgBuyName],
+  
+];
+export const MissingWalletError = new Error("wallet is required");
+
+export const registry = new Registry(<any>types);
+
+const defaultFee = {
+  amount: [],
+  gas: "200000",
+};
+
+interface TxClientOptions {
+  addr: string
+}
+
+interface SignAndBroadcastOptions {
+  fee: StdFee,
+  memo?: string
+}
+
+const txClient = async (wallet: OfflineSigner, { addr: addr }: TxClientOptions = { addr: "http://localhost:26657" }) => {
+  if (!wallet) throw MissingWalletError;
+  let client;
+  if (addr) {
+    client = await SigningStargateClient.connectWithSigner(addr, wallet, { registry });
+  }else{
+    client = await SigningStargateClient.offline( wallet, { registry });
+  }
+  const { address } = (await wallet.getAccounts())[0];
+
+  return {
+    signAndBroadcast: (msgs: EncodeObject[], { fee, memo }: SignAndBroadcastOptions = {fee: defaultFee, memo: ""}) => client.signAndBroadcast(address, msgs, fee,memo),
+    msgSetName: (data: MsgSetName): EncodeObject => ({ typeUrl: "/nameservice.nameservice.MsgSetName", value: MsgSetName.fromPartial( data ) }),
+    msgDeleteName: (data: MsgDeleteName): EncodeObject => ({ typeUrl: "/nameservice.nameservice.MsgDeleteName", value: MsgDeleteName.fromPartial( data ) }),
+    msgBuyName: (data: MsgBuyName): EncodeObject => ({ typeUrl: "/nameservice.nameservice.MsgBuyName", value: MsgBuyName.fromPartial( data ) }),
+    
+  };
+};
+
+interface QueryClientOptions {
+  addr: string
+}
+
+const queryClient = async ({ addr: addr }: QueryClientOptions = { addr: "http://localhost:1317" }) => {
+  return new Api({ baseUrl: addr });
+};
+
+export {
+  txClient,
+  queryClient,
+};
