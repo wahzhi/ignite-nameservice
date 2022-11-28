667e87ea09ed649b47776f17013627f1fc1fd1e6
wahzhi <fre51bor@gmail.com>
2022-11-28 06:21:44 -0800

    update keepers methods :

--- x/nameservice/keeper/msg_server_delete_name.go
+++ x/nameservice/keeper/msg_server_delete_name.go
@@ -1,17 +1,31 @@
 package keeper
 
 import (
-	"context"
+    "context"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"nameservice/x/nameservice/types"
+    sdk "github.com/cosmos/cosmos-sdk/types"
+    sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
+    "nameservice/x/nameservice/types"
 )
 
 func (k msgServer) DeleteName(goCtx context.Context, msg *types.MsgDeleteName) (*types.MsgDeleteNameResponse, error) {
-	ctx := sdk.UnwrapSDKContext(goCtx)
+    ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+    // Try getting name information from the store
+    whois, isFound := k.GetWhois(ctx, msg.Name)
 
-	return &types.MsgDeleteNameResponse{}, nil
+    // If a name is not found, throw an error
+    if !isFound {
+        return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "Name doesn't exist")
+    }
+
+    // If the message sender address doesn't match the name owner, throw an error
+    if !(whois.Owner == msg.Creator) {
+        return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "Incorrect Owner")
+    }
+
+    // Otherwise, remove the name information from the store
+    k.RemoveWhois(ctx, msg.Name)
+    return &types.MsgDeleteNameResponse{}, nil
 }
--- x/nameservice/keeper/msg_server_set_name.go
+++ x/nameservice/keeper/msg_server_set_name.go
@@ -1,17 +1,35 @@
 package keeper
 
 import (
-	"context"
+    "context"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"nameservice/x/nameservice/types"
+    sdk "github.com/cosmos/cosmos-sdk/types"
+    sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
+    "nameservice/x/nameservice/types"
 )
 
 func (k msgServer) SetName(goCtx context.Context, msg *types.MsgSetName) (*types.MsgSetNameResponse, error) {
-	ctx := sdk.UnwrapSDKContext(goCtx)
+    ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+    // Try getting name information from the store
+    whois, _ := k.GetWhois(ctx, msg.Name)
 
-	return &types.MsgSetNameResponse{}, nil
+    // If the message sender address doesn't match the name owner, throw an error
+    if !(msg.Creator == whois.Owner) {
+        return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "Incorrect Owner")
+    }
+
+    // Otherwise, create an updated whois record
+    newWhois := types.Whois{
+        Index: msg.Name,
+        Name:  msg.Name,
+        Value: msg.Value,
+        Owner: whois.Owner,
+        Price: whois.Price,
+    }
+
+    // Write whois information to the store
+    k.SetWhois(ctx, newWhois)
+    return &types.MsgSetNameResponse{}, nil
 }
